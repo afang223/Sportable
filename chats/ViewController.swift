7e7acfb3eb712f206d0919810c2368052d352c43
Andy Fang <andyfang486@berkeley.edu>
2020-06-04 20:39:12 -0700

    Initial Commit

---
+++ chats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chats
+//
+//  Created by Andy Fang on 6/4/20.
+//  Copyright © 2020 Andy Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chatsTests/chatsTests.swift
@@ -0,0 +1,34 @@
+//
+//  chatsTests.swift
+//  chatsTests
+//
+//  Created by Andy Fang on 6/4/20.
+//  Copyright © 2020 Andy Fang. All rights reserved.
+//
+
+import XCTest
+@testable import chats
+
+class chatsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
